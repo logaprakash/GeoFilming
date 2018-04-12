2b4046de7012494e125f95eb414a9d62c0e1cdc1
Sakshi Jain <Jain-Sakshi@users.noreply.github.com>
2018-04-12 19:10:59 +0530

    Accuracy Score via CrossValidation

--- Logictic_Regression.py
+++ Logictic_Regression.py
@@ -5,6 +5,7 @@ Logistic Regresion
 Logistic Regression
 """
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
 import pandas as pd
 
 dataset_df = pd.read_pickle('dataset_df.p')
@@ -14,4 +15,6 @@ X_train, X_test, Y_train, Y_test = data[:800000], data[800000:], target[:800000]
 logReg = LogisticRegression()
 logReg.fit(X_train, Y_train)
 logReg_predictions = logReg.predict(X_test)
-logReg.score(X_test, Y_test)
+
+scores = cross_val_score(logReg, data, target, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
